218f3e724e4e80acadd899d20430411d1d299062
bebopmot <bebopmotinc@gmail.com>
2025-04-22 19:05:35 +0200

    navbar and logo

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -3,17 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import NavBar from './components/NavBar'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        Hello world
-      </div>
-      <p className="read-the-docs">
-      
-      </p>
+    <header>
+<NavBar />
+    </header>
+     
     </>
   )
 }
---
+++ my-first-app/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+function NavBar(){
+return( <div className = "NavBar">
+<h1>My First App</h1>
+<img className = "logo" src = "./src/assets/Free.png"/>
+
+</div>
+)}
+
+
+export default NavBar;
